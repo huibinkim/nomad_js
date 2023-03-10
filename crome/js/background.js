d4c07e985686672a4dd769f3ee13ed5e76fac38c
[huibinKim] <abc00127@naver.com>
2023-03-10 12:22:53 +0900

    230310 backgorund img를 html의 body에 랜덤하게 넣기

---
+++ crome/js/background.js
@@ -0,0 +1,12 @@
+const images = [
+    "bg_01.jpeg", "bg_02.png", "bg_03.jpeg", "bg_04.jpeg"
+];
+const todayImg = images[Math.floor(Math.random() * images.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${todayImg}`;
+
+document.body.appendChild(bgImg);
+//append() 맨뒤에 추가
+//prepand() 맨앞에 추가
