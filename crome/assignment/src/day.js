a49656ff66b5be149cd722277e446d23be773c61
[huibinKim] <abc00127@naver.com>
2023-03-30 12:01:55 +0900

    버튼 클릭시 body 배경에 그라데이션 넣기 과제

--- crome/assignment/src/day.js
+++ crome/assignment/src/day.js
@@ -26,3 +26,36 @@ waitDay();
 setInterval(waitDay, 1000);
 
 //1일 24시간 24*60분 24*60*60초 24*60*60*1000ms
+
+//background color linear-gradiant로 바꾸기
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+const btn = document.querySelector("button");
+const body = document.querySelector("body");
+
+function onClick() {
+  const randomOne = colors[Math.floor(Math.random() * colors.length)];
+  const randomTwo = colors[Math.floor(Math.random() * colors.length)];
+  body.style.backgroundImage = `linear-gradient(0.25turn, ${randomOne},${randomTwo})`;
+  console.log(randomOne, randomTwo);
+}
+
+btn.addEventListener("click", onClick);
