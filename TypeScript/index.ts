b5e11533297cc1983b2eed0cc2cab0d2e2ac0e94
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-04-05 12:26:20 +0900

    call signature, overloading

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -6,6 +6,9 @@
 
 //player라는 변수안의 object를 각각 어떤 타입으로 할 것인지 저장한다, 그러나 name은 필수적으로 age는 선택적으로 하고 싶을때
 // ?를 붙여 표시할 수 잇다.
+/**
+ * 
+
 type Age = number;
 type Name = string;
 type Player = {
@@ -56,3 +59,41 @@ function hellog(name: string | number) {
     name; //never
   }
 }
+
+//#3.0 강의시작
+//call signatures : 함수 위에 마우스를 올렸을 때 보게 되는 것을 뜻함. 인자의 타입과 함수의 반환 타입을 알려줌.
+// function add(a:number, b:number){
+//   return a + b
+// }
+type Add = (a: number, b: number) => number;
+const add: Add = (a, b) => a + b;
+ */
+//overloading : 함수가 여러개의 call signatures를 가지고 있을 때 발생한다.
+//Function(=Method) Overloading은 직접 작성하기보다 외부 라이브러리에 자주 보이는 형태로, 하나의 함수가 복수의 Call Signature를 가질 때 발생한다
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+add(1, 2);
+add(1, 2, 3);
+
+//예시
+type Config = {
+  path: string;
+  state: object;
+};
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+const push: Push = (config) => {
+  if (typeof config === "string") console.log(config);
+  else {
+    console.log(config.path);
+  }
+};
